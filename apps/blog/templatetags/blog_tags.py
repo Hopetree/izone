be1ec25695442c7a3f4e5122c340013a77d2b5ac
Hopegoing <zlwork2014@gmail.com>
2018-04-09 21:31:09 +0800

    添加友链到首页

--- apps/blog/templatetags/blog_tags.py
+++ apps/blog/templatetags/blog_tags.py
@@ -1,7 +1,7 @@
 # 创建了新的tags标签文件后必须重启服务器
 
 from django import template
-from ..models import Article, Category, Tag, Carousel
+from ..models import Article, Category, Tag, Carousel, FriendLink
 from django.db.models.aggregates import Count
 from django.utils.html import mark_safe
 
@@ -10,7 +10,7 @@ register = template.Library()
 
 # 文章相关标签函数
 @register.simple_tag
-def get_article_list(sort=None,num=None):
+def get_article_list(sort=None, num=None):
     '''获取指定排序方式和指定数量的文章'''
     if sort:
         if num:
@@ -21,38 +21,43 @@ def get_article_list(sort=None,num=None):
     return Article.objects.all()
 
 
-
 @register.simple_tag
 def keywords_to_str(art):
     '''将文章关键词变成字符串'''
     keys = art.keywords.all()
     return ','.join([key.name for key in keys])
 
+
 @register.simple_tag
 def get_tag_list():
     '''返回标签列表'''
     return Tag.objects.annotate(total_num=Count('article')).filter(total_num__gt=0)
+
 
 @register.simple_tag
 def get_category_list():
     '''返回分类列表'''
     return Category.objects.annotate(total_num=Count('article')).filter(total_num__gt=0)
 
+
 @register.inclusion_tag('blog/tags/article_list.html')
 def load_article_summary(articles):
     '''返回文章列表模板'''
-    return {'articles':articles}
+    return {'articles': articles}
 
-@register.inclusion_tag('blog/tags/pagecut.html',takes_context=True)
+
+@register.inclusion_tag('blog/tags/pagecut.html', takes_context=True)
 def load_pages(context):
     '''分页标签模板，不需要传递参数，直接继承参数'''
     return context
+
 
 # 其他函数
 @register.simple_tag
 def get_carousel_list():
     '''获取轮播图片列表'''
     return Carousel.objects.all()
+
 
 @register.simple_tag
 def get_star(num):
@@ -73,16 +78,24 @@ def get_star_title(num):
     }
     return the_dict[num]
 
+
 @register.simple_tag
-def my_highlight(text,q):
+def my_highlight(text, q):
     '''自定义标题搜索词高亮函数'''
     try:
-        r = text.replace(q,'<span class="highlighted">{}</span>'.format(q))
+        r = text.replace(q, '<span class="highlighted">{}</span>'.format(q))
         return mark_safe(r)
     except:
         return text
+
 
 @register.simple_tag
 def get_request_param(request, param, default=None):
     '''获取请求的参数'''
     return request.POST.get(param) or request.GET.get(param, default)
+
+
+@register.simple_tag
+def get_friends():
+    '''获取活跃的友情链接'''
+    return FriendLink.objects.filter(is_show=True, is_active=True)
