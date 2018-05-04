c76ddf910e088b02f23c834979e51436e3a7778a
Hopegoing <zlwork2014@gmail.com>
2018-05-04 23:27:47 +0800

    change settings

--- izone/settings.py
+++ izone/settings.py
@@ -212,4 +212,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20
 }
