d5efda4c42d1ca8c0b366acb7ca5a76540ae5dd2
Hopetree <zlwork2014@163.com>
2019-01-29 19:13:21 +0800

    https

--- apps/oauth/templatetags/oauth_tags.py
+++ apps/oauth/templatetags/oauth_tags.py
@@ -8,3 +8,9 @@ register = template.Library()
 def get_user_avatar_tag(user):
     '''返回用户的头像，是一个img标签'''
     return {'user':user}
+
+
+@register.simple_tag
+def http_to_https(link):
+    '''将http链接替换成https，目的是将微博的头像图床地址换成HTTPS'''
+    return link.replace('http://','https://')
