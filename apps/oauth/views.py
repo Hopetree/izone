b65aa588586bf0b58b9c4e4db3d151afb00f3dc9
Hopetree <zlwork2014@163.com>
2019-02-28 23:01:12 +0800

    用户头像更新之前删除旧的头像

--- apps/oauth/views.py
+++ apps/oauth/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 from .forms import ProfileForm
 from django.contrib import messages
+import os
 # Create your views here.
 
 @login_required
@@ -12,9 +13,12 @@ def profile_view(request):
 @login_required
 def change_profile_view(request):
     if request.method == 'POST':
+        old_avatar_file = request.user.avatar.path
         # 上传文件需要使用request.FILES
         form = ProfileForm(request.POST,request.FILES,instance=request.user)
         if form.is_valid():
+            if os.path.exists(old_avatar_file):
+                os.remove(old_avatar_file)
             form.save()
             # 添加一条信息,表单验证成功就重定向到个人信息页面
             messages.add_message(request,messages.SUCCESS,'个人信息更新成功！')
