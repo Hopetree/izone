178e0817012a9611b3dfe607d838a5d7a9e2be1a
Hopetree <zlwork2014@163.com>
2019-03-10 00:22:14 +0800

    tool url

--- apps/tool/urls.py
+++ apps/tool/urls.py
@@ -7,7 +7,7 @@ from .views import (Toolview, BD_pushview, BD_pushview_site, Link_testview,
 urlpatterns = [
     url(r'^$', Toolview, name='total'),  # 工具汇总页
     url(r'^baidu-linksubmit/$', BD_pushview, name='baidu_push'),  # 百度主动推送
-    url(r'^baidu-linksubmit-for-sitemap/$', BD_pushview_site, name='baidu_push_site'),  # 百度主动推送sitemap
+    url(r'^baidu-linksubmit-sitemap/$', BD_pushview_site, name='baidu_push_site'),  # 百度主动推送sitemap
     url(r'^link-test/$', Link_testview, name='link_test'),  # 友链检测
     url(r'^regex/$', regexview, name='regex'),  # 正则表达式在线
     url(r'^user-agent/$', useragent_view, name='useragent'),  # user-agent生成器
