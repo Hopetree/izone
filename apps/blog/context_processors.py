e13c5ab52eb49cbdf96a43f8dc79a699ac17f69a
Hopetree <zlwork2014@163.com>
2019-08-16 18:49:25 +0800

    去掉首页缓存

--- apps/blog/context_processors.py
+++ apps/blog/context_processors.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from django.conf import settings
+from .utils import site_full_url
+
 
 # 自定义上下文管理器
 def settings_info(request):
     return {
-        'site_end_title':settings.SITE_END_TITLE,
-        'site_description':settings.SITE_DESCRIPTION,
-        'site_keywords':settings.SITE_KEYWORDS,
-        'tool_flag':settings.TOOL_FLAG,
-        'api_flag':settings.API_FLAG,
-        'cnzz_protocol':settings.CNZZ_PROTOCOL,
-        'beian':settings.BEIAN,
-        'my_github':settings.MY_GITHUB,
-        'site_verification':settings.MY_SITE_VERIFICATION,
+        'site_end_title': settings.SITE_END_TITLE,
+        'site_description': settings.SITE_DESCRIPTION,
+        'site_keywords': settings.SITE_KEYWORDS,
+        'tool_flag': settings.TOOL_FLAG,
+        'api_flag': settings.API_FLAG,
+        'cnzz_protocol': settings.CNZZ_PROTOCOL,
+        'beian': settings.BEIAN,
+        'my_github': settings.MY_GITHUB,
+        'site_verification': settings.MY_SITE_VERIFICATION,
+        'site_url': site_full_url()
     }
