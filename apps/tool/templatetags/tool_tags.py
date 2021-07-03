7fe8afa552ffedc3a8669f1381448900fcf85a6b
hopetree <zlwork2014@163.com>
2021-07-03 14:03:25 +0800

    新增自定义工具展示

--- apps/tool/templatetags/tool_tags.py
+++ apps/tool/templatetags/tool_tags.py
@@ -4,7 +4,7 @@ from django.db.models.aggregates import Count
 from django.templatetags.static import static
 from django.urls import reverse
 
-from ..models import ToolCategory, ToolLink
+from ..models import ToolCategory
 from ..utils import IZONE_TOOLS
 
 register = template.Library()
@@ -13,7 +13,8 @@ register = template.Library()
 @register.simple_tag
 def get_toolcates():
     '''获取所有工具分类，只显示有工具的分类'''
-    return ToolCategory.objects.annotate(total_num=Count('toollink')).filter(total_num__gt=0)
+    return ToolCategory.objects.annotate(total_num=Count('toollink')).filter(
+        total_num__gt=0)
 
 
 @register.simple_tag
