97122142479347e2839e3fada4fd17db7d4e37f9
hopetree <zlwork2014@163.com>
2023-04-23 09:52:36 +0800

    头像文件不存在则显示默认头像

--- apps/oauth/models.py
+++ apps/oauth/models.py
@@ -1,11 +1,15 @@
+import os
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
 
+from django.conf import settings
+
 
 class Ouser(AbstractUser):
-    link = models.URLField('个人网址',blank=True,help_text='提示：网址必须填写以http开头的完整形式')
+    link = models.URLField('个人网址', blank=True, help_text='提示：网址必须填写以http开头的完整形式')
     avatar = ProcessedImageField(upload_to='avatar/%Y/%m/%d',
                                  default='avatar/default.png',
                                  verbose_name='头像',
@@ -19,3 +23,13 @@ class Ouser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+    def get_avatar_or_default_url(self):
+        """
+        校验图像地址，如果文件不存在则使用默认的头像
+        """
+        avatar_path = self.avatar.path
+        if os.path.exists(avatar_path):
+            return self.avatar.url
+        else:
+            return settings.MEDIA_URL + 'avatar/default.png'
