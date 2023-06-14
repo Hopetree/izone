40cb5b6d616cf7dc1969bf3e1b290e824d75869d
hopetree <zlwork2014@163.com>
2023-06-14 12:41:00 +0800

    优化代码块样式

--- apps/blog/static/blog/js/base.js
+++ apps/blog/static/blog/js/base.js
@@ -68,7 +68,7 @@ function addDarkTheme() {
         link.type = 'text/css';
         link.id = "theme-css-dark"; // 加上id方便后面好查找到进行删除
         link.rel = 'stylesheet';
-        link.href = '/static/blog/css/night.css?20230611.03';
+        link.href = '/static/blog/css/night.css?20230614.03';
         $("head").append(link);
     }
     const changeThemeI = $('#change-theme i');
