3cd186c5d4e7046cdd1a84ca84e291bea17763c8
hopetree <zlwork2014@163.com>
2023-07-02 00:15:50 +0800

    添加友情链接检查定时任务

--- apps/easytask/actions.py
+++ apps/easytask/actions.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+import requests
 from markdown import Markdown
 from markdown.extensions.toc import TocExtension  # 锚点的拓展
 from markdown.extensions.codehilite import CodeHiliteExtension
 from django.core.cache import cache
 from django.utils.text import slugify
 
-from blog.models import Article
+from blog.models import Article, FriendLink
 from blog.utils import CustomHtmlFormatter
 
 
@@ -33,4 +34,41 @@ def update_article_cache():
             cache.set(md_key, (md.convert(obj.body), md.toc), 3600 * 24 + 10 * done_num)
             done_num += 1
     data = {'total': total_num, 'done': done_num}
+    return data
+
+
+def check_friend_links():
+    """
+    检查友链:
+        1、检查当前显示的友链，请求友链，将非200的友链标记为不显示
+        2、检查当前不显示的友链，请求友链，将200返回的标记为显示
+    @return:
+    """
+
+    def get_link_status(url):
+        try:
+            status_code = requests.get(url, timeout=5, verify=False).status_code
+        except Exception:
+            status_code = 500
+        return status_code
+
+    active_num = 0
+    to_not_show = 0
+    to_show = 0
+    active_friend_list = FriendLink.objects.filter(is_active=True)
+    for active_friend in active_friend_list:
+        active_num += 1
+        if active_friend.is_show is True:
+            code = get_link_status(active_friend.link)
+            if code != 200:
+                active_friend.is_show = False
+                active_friend.save(update_fields=['is_show'])
+                to_not_show += 1
+        else:
+            code = get_link_status(active_friend.link)
+            if code == 200:
+                active_friend.is_show = True
+                active_friend.save(update_fields=['is_show'])
+                to_show += 1
+    data = {'active_num': active_num, 'to_not_show': to_not_show, 'to_show': to_show}
     return data
--- apps/easytask/tasks.py
+++ apps/easytask/tasks.py
@@ -3,7 +3,7 @@ import time
 
 from celery import shared_task
 
-from .actions import update_article_cache
+from .actions import (update_article_cache, check_friend_links)
 from .utils import TaskResponse
 
 
@@ -22,4 +22,16 @@ def update_cache():
     response = TaskResponse()
     article_result = update_article_cache()
     response.data['article'] = article_result
+    return response.as_dict()
+
+
+@shared_task
+def check_friend():
+    """
+    检查友链
+    @return:
+    """
+    response = TaskResponse()
+    check_result = check_friend_links()
+    response.data = check_result
     return response.as_dict()
