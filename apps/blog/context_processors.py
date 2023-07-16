d39d18342bfc62137591d24aa675857102d5eb16
hopetree <zlwork2014@163.com>
2023-07-16 18:24:18 +0800

    添加专题页面

--- apps/blog/context_processors.py
+++ apps/blog/context_processors.py
@@ -7,7 +7,7 @@ from .utils import (site_full_url, get_site_create_day)
 # 静态文件版本（只收集常改的，不常改的直接在页面改），每次更新了静态文件就更新一下这个版本
 # todo 可以做成自动化，每次拉git代码的时候检查是否更新了某个静态文件，自动更新版本
 STATIC_VERSION = {
-    'css_blog_base': '20230716.01',
+    'css_blog_base': '20230716.02',
     'css_blog_detail': '20230713.09',
     'css_blog_night': '20230713.06',
 
