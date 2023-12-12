10038a8cb79046f41aee6a69164a5278d021b8d8
hopetree <zlwork2014@163.com>
2023-12-12 18:48:12 +0800

    预测今日此时的阅读量

--- apps/blog/templatetags/dashboard.py
+++ apps/blog/templatetags/dashboard.py
@@ -9,24 +9,62 @@ from blog.models import ArticleView, Article
 register = template.Library()
 
 
+def get_day_of_week(date_string):
+    # 将输入的日期字符串转换为日期对象
+    date_object = datetime.strptime(date_string, '%Y%m%d')
+    # 获取星期几的数字（0代表星期一，1代表星期二，以此类推）
+    day_of_week = date_object.weekday()
+    # 映射数字到星期几的字符串
+    days_of_week = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+    day_str = days_of_week[day_of_week]
+
+    return day_str
+
+
+def get_today_views_by_forecast():
+    """
+    预测今天的访问量总数，根据昨天每小时和今天已经生成的小时数据进行预测，数据越多应该越接近
+    @return:
+    """
+    result = '-'
+    yes_date_str = (datetime.today() - timedelta(days=1)).strftime('%Y%m%d')  # 昨天
+    thi_date_str = datetime.today().strftime('%Y%m%d')  # 今天
+    this_hour = datetime.now().strftime('%H')
+    yes_hours_data = get_hours_data_by_date(yes_date_str)
+    thi_hours_data = get_hours_data_by_date(thi_date_str)
+    # 昨天数据必须满24小时，今天数据最少有一个小时的才能计算
+    if len(yes_hours_data) == 24 and len(thi_hours_data) > 0:
+        yes_total_views = yes_hours_data['23']  # 昨日总计
+        yes_done_views = yes_hours_data[this_hour]  # 昨日此时
+        thi_done_views = thi_hours_data[this_hour]  # 今日此时
+        # print(yes_total_views, yes_done_views, thi_done_views)
+        result = int(yes_total_views * thi_done_views / yes_done_views) - yes_hours_data['23']
+    return result
+
+
 @register.simple_tag
-def get_views_data_from_redis(t1, t2):
+def get_views_data_from_redis(t1, t2, t3):
     """
     从redis里面获取文章访问量数据，获取不到则返回空格式
     @return:
     """
+    thi_date_str = datetime.today().strftime('%Y%m%d')
     redis_key = RedisKeys.views_statistics
     redis_data = cache.get(redis_key)
     days = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
-    data = [['product', t1, t2]]
+    data = [['product', t1, t2, t3]]
     if not redis_data:
         for day in days:
-            data.append([day, '-', '-'])
+            data.append([day, '-', '-', '-'])
     else:
         last_week = redis_data['last_week_views']
         this_week = redis_data['this_week_views']
         for day in days:
-            data.append([day, this_week.get(day, '-'), last_week.get(day, '-')])
+            if get_day_of_week(thi_date_str) == day:  # 如果是今天的数据，则预测今天
+                forecast_views = get_today_views_by_forecast()
+            else:
+                forecast_views = '-'
+            data.append([day, this_week.get(day, '-'), last_week.get(day, '-'), forecast_views])
     return data
 
 
