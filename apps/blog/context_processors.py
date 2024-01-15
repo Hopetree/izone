7e51f325921358ee659be5ff2743936d8aa33553
hopetree <zlwork2014@163.com>
2024-01-15 09:11:32 +0800

    不常用的静态文件压缩

--- apps/blog/context_processors.py
+++ apps/blog/context_processors.py
@@ -7,16 +7,16 @@ from .utils import (site_full_url, get_site_create_day)
 # 静态文件版本（只收集常改的，不常改的直接在页面改），每次更新了静态文件就更新一下这个版本
 # todo 可以做成自动化，每次拉git代码的时候检查是否更新了某个静态文件，自动更新版本
 STATIC_VERSION = {
-    'css_blog_base': '20231213.02',
-    'css_blog_detail': '20230916.03',
-    'css_blog_night': '20231213.31',
+    'css_blog_base': '20200115.01',
+    'css_blog_detail': '20200115.01',
+    'css_blog_night': '20200115.01',
 
-    'js_blog_base': '20230718.01',
-    'js_blog_article': '20230713.01',
-    'js_blog_code': '20230730.01',
+    'js_blog_base': '20200115.01',
+    'js_blog_article': '20200115.01',
+    'js_blog_code': '20200115.01',
 
-    'css_tool_tool': '20230713.01',
-    'js_tool_tool': '20231226.01',
+    'css_tool_tool': '20200115.01',
+    'js_tool_tool': '20200115.01',
 }
 
 
