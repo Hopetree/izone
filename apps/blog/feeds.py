89161932695eea74869c17041f2649d92e102c26
hopetree <zlwork2014@163.com>
2024-01-29 09:41:27 +0800

    添加代码组支持

--- apps/blog/feeds.py
+++ apps/blog/feeds.py
@@ -23,5 +23,4 @@ class AllArticleRssFeed(Feed):
 
     # 显示的内容的描述
     def item_description(self, item):
-        # return item.body_to_markdown()
-        return escape(str(item.summary))
+        return item.body_to_markdown()
