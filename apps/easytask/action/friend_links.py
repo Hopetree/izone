a455c19cd5d2113bfbd2f527889ffc4150b14f29
hopetree <zlwork2014@163.com>
2024-09-23 18:10:55 +0800

    feat: 定时任务异步改造

---
+++ apps/easytask/action/friend_links.py
@@ -0,0 +1,93 @@
+import re
+import asyncio
+import aiohttp
+from blog.models import FriendLink
+
+
+class LinkChecker:
+    def __init__(self, site_link=None, white_list=None):
+        """
+        :param site_link: 需要在友链页面中检验的外链
+        :param white_list: 白名单中的友链将不会被校验
+        """
+        self.site_link = site_link
+        self.white_list = white_list or []
+        self.result = {
+            'active_num': 0,
+            'to_not_show': 0,
+            'to_show': 0
+        }
+
+    async def get_link_status(self, session, url):
+        """
+        异步获取链接状态码和网页内容
+        :param session: aiohttp 的 session 实例
+        :param url: 需要请求的 URL
+        :return: 状态码和网页文本内容
+        """
+        try:
+            async with session.get(url, timeout=5) as response:
+                code = response.status
+                text = await response.text()
+                return code, text
+        except Exception as e:
+            return None, str(e)
+
+    async def check_link(self, session, active_friend):
+        """
+        校验单个友链的状态
+        :param session: aiohttp 的 session 实例
+        :param active_friend: FriendLink 模型实例
+        :return: None
+        """
+        if active_friend.name in self.white_list:
+            return
+
+        self.result['active_num'] += 1
+        if active_friend.is_show:
+            code, text = await self.get_link_status(session, active_friend.link)
+            if code != 200:
+                active_friend.is_show = False
+                active_friend.not_show_reason = f'网页请求返回{code}'
+                self.result['to_not_show'] += 1
+            elif self.site_link and not re.findall(self.site_link, text):
+                active_friend.is_show = False
+                active_friend.not_show_reason = f'网站未设置本站外链'
+                self.result['to_not_show'] += 1
+            active_friend.save(update_fields=['is_show', 'not_show_reason'])
+        else:
+            code, text = await self.get_link_status(session, active_friend.link)
+            if code == 200:
+                if not self.site_link or re.findall(self.site_link, text):
+                    active_friend.is_show = True
+                    active_friend.not_show_reason = ''
+                    self.result['to_show'] += 1
+                    active_friend.save(update_fields=['is_show', 'not_show_reason'])
+
+    async def check_all_links(self):
+        """
+        异步检查所有友链
+        """
+        active_friend_list = FriendLink.objects.filter(is_active=True)
+        async with aiohttp.ClientSession() as session:
+            tasks = [self.check_link(session, friend) for friend in active_friend_list]
+            await asyncio.gather(*tasks)
+
+    def run(self):
+        """
+        启动异步任务，检查友链
+        """
+        asyncio.run(self.check_all_links())
+        return self.result
+
+
+def action_check_friend_links(site_link=None, white_list=None):
+    checker = LinkChecker(site_link, white_list)
+    result = checker.run()
+    return result
+
+
+# 用法示例
+if __name__ == '__main__':
+    r = action_check_friend_links(site_link='https://tendcode.com', white_list=['TendCode'])
+    print(r)
